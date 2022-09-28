259e7f7637cf42af74d640141aaa3016e4fdf7dc
HajraRafiq <hajrarafiq1828@gmail.com>
2022-09-28 21:56:57 +0500

    Constructors inside class

---
+++ Day017/constructors.py
@@ -0,0 +1,20 @@
+#Creating attributes with the help of constructors
+#constructors allows us to specify what should happen when our object is being constructed
+#It is also called initializing an object
+
+class User:
+    
+    def __init__(self,user_id,username): #initializing attributed
+        #init function is going to be called every time when a new object is created
+        self.id = user_id
+        self.username=username
+        self.followers = 0
+        
+
+user_1=User("001","Hajra")
+print(user_1.id)
+
+user_2=User("002","Ammar")
+
+print(user_2.username)
+print(user_2.followers)
