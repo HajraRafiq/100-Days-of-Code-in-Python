be1a7498a84ad45537b9e862c5e73494635644f1
HajraRafiq <hajrarafiq1828@gmail.com>
2022-09-28 22:03:51 +0500

    Main Quiz File

---
+++ Day017_Quiz_Project/quiz.py
@@ -0,0 +1,15 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for ques in question_data:
+    ques_text = ques["text"]
+    ques_answer = ques["answer"]
+    new_ques = Question(ques_text,ques_answer)
+    question_bank.append(new_ques)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+
+    quiz.next_ques()
