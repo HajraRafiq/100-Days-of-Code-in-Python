426e0a5c0d83058b535adab0f576faf189283b33
HajraRafiq <hajrarafiq1828@gmail.com>
2022-09-30 01:25:15 +0500

    Getting started with turtle

---
+++ Day018/turtle_main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle , Screen
+
+my_turtle = Turtle()
+my_turtle.shape('turtle')
+my_turtle.color('purple3')
+my_turtle.forward(200)
+my_turtle.right(90)
+
+
+
+screen = Screen()
+screen.bgcolor('black')
+screen.exitonclick()
