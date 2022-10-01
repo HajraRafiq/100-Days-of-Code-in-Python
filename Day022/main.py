7b0c1344bf349715a642ba107672fd2607bacc48
HajraRafiq <hajrarafiq1828@gmail.com>
2022-10-01 21:36:25 +0500

    Final Code for Building Pong

---
+++ Day022/main.py
@@ -0,0 +1,70 @@
+from turtle import Turtle , Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
+# Setting up the screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width = 800 , height = 600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle= Paddle((350,0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+#Keystrokes
+screen.listen()
+screen.onkey(r_paddle.go_up , "Up")
+screen.onkey(r_paddle.go_down , "Down")
+screen.onkey(l_paddle.go_up , "a")
+screen.onkey(l_paddle.go_down , "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #Collision Detection with wall
+    if ball.ycor()>280 or ball.ycor()<-280:
+        ball.bounce_y()
+
+    #Collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor()>320 or ball.distance(l_paddle) < 50 and ball.xcor()>-320:
+        ball.bounce_x()
+    
+    #Detect when right paddle misses
+    if ball.xcor()>380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect when left paddle misses
+    if ball.xcor()<-380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+
+
+
+
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
