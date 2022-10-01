3ca273efb2d7bbc62ae82170c5533b548486d381
HajraRafiq <hajrarafiq1828@gmail.com>
2022-10-01 17:33:49 +0500

    Some Concepts of Inheritance in Python

--- Day021/Class_Inheritance.py
+++ Day021/Class_Inheritance.py
@@ -5,14 +5,16 @@ class Animal:
     def breathe(self):
         print("Inhale , Exhale.")
     
+# Introducing a new Fish class and Inheriting the functions and behavior from Animal Class
+
 class Fish(Animal):
     def __init__(self):
-        super().__init__()
+        super().__init__()  #super keyword is used to inherit everything from Animal CLass
     def swim(self):
         print("Moving in Water")
 
     def breathe(self):
-        super().breathe()
+        super().breathe() # Inheriting breathe function from Animal Class and adding another behavior for Fish
         print("Doing this underwater")
 
 nemo = Fish()
