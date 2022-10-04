2eba7a0ca21bad2f26c92caf8747c0d5d10de72c
HajraRafiq <hajrarafiq1828@gmail.com>
2022-10-04 23:30:52 +0500

    US State Game in python

--- Day025_US_State_Game/main.py
+++ Day025_US_State_Game/main.py
@@ -15,17 +15,17 @@ guessed_states = []
 while len(guessed_states)<50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct" , prompt="What's the another state is?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        
+        missing_states = [state for state in states if state not in guessed_states]
+
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("States_to_learn.csv")
 
         break
+        
     if answer_state in states:
         guessed_states.append(answer_state)
-        t = turtle.Turtle(300,200)
+        t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
